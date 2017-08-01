48c4019d492b9434ac4db278a71188c87b4af9f2
Petar Obradović <spideyfusion@gmail.com>
2017-08-01 08:55:11 +0200

    Add dictionary specific patches

--- script/update.sh
+++ script/update.sh
@@ -5,4 +5,7 @@ curl -o dict/hr_HR.aff https://raw.githubusercontent.com/krunose/hr-hunspell/mas
 curl -o dict/hr_HR.dic https://raw.githubusercontent.com/krunose/hr-hunspell/master/hr_HR.dic
 curl -o README_hr_HR.txt https://raw.githubusercontent.com/krunose/hr-hunspell/master/README_hr_HR.txt
 
+# Apply dictionary specific patches
+sed -i -e "$(<src/dic.patch)" dict/hr_HR.dic
+
 echo -e "\e[32mDone!\e[39m"
